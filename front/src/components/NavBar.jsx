2675b53692c4c1581544b4253cd3617b2b09ca86
Sebastian Soria <soriagorgoroso@gmail.com>
2022-05-24 02:48:35 -0300

    NavBar v3
    Componente NavBar bastante terminado con react router en los links responsive y con el efecto de cambio de color al scrollear 80px

--- front/src/components/NavBar.jsx
+++ front/src/components/NavBar.jsx
@@ -1,16 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./header.css";
-import { MenuItems } from "./MenuItems";
 import NavLink, { Link } from "react-router-dom";
 import {
   Container,
   Navbar,
   Nav,
   NavDropdown,
-  Offcanvas,
-  Form,
-  FormControl,
-  Button,
 } from "react-bootstrap";
 
 function NavBar() {
@@ -26,70 +21,45 @@ function NavBar() {
 
   return (
     <>
-      <nav className={navBar ? "navBar active" : "navBar"}>
-        <a className="navBarLogo" href="/">
-          <img src="img/core-img/logo.png" alt="" />
-        </a>
-        <ul>
-          {MenuItems.map((item, index) => {
-            return (
-              <li>
-                <a className={item.cName} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-      {/* <Navbar
-        className={navBar ? "navBar active" : "navBar"}
+     <Navbar
+         className={
+            navBar
+              ? "navBar active navbar fixed-top navbar-dark p-md-3"
+              : "navBar navbar fixed-top navbar-dark p-md-3"}
         id=""
         collapseOnSelect
         expand="lg"
         // bg="dark"
-        sticky="top"
+        // sticky="top"
         variant="dark"
       >
         <Container className="">
           <Navbar.Brand>
             {" "}
-            <a href="index.php" className="">
+            <Link to="/" className="">
               <img src="img/core-img/logo.png" alt="" />
-            </a>
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto pt-2">
-              <Nav.Link href="/">Inicio</Nav.Link>
-              <Nav.Link href="product">Productos</Nav.Link>
-              <Nav.Link href="event">Eventos</Nav.Link>
-              <Nav.Link href="contact">Contactos</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Link className="text-white m-3" to="/">Inicio</Link>
+              <Link className="text-white m-3" to="product">Productos</Link>
+              <Link className="text-white m-3" to="event">Eventos</Link>
+              <Link className="text-white m-3" to="contact">Contactos</Link>
+            
             </Nav>
-            <Nav>
-              <Nav.Link className="login-register-btn mr-50" href="#deets">
+         
+              <Link  className="text-white m-3" to="#deets">
                 Login
-              </Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                <span class="icon-shopping-cart">2</span>
-              </Nav.Link>
-            </Nav>
+              </Link>
+                <Link eventKey={2} className="text-white"  to="/cart">
+               <i class="fa-solid fa-cart-shopping">2</i>
+              </Link>
+            
           </Navbar.Collapse>
         </Container>
-      </Navbar> */}
+      </Navbar>  
     </>
   );
 }
