8198b17465b8e7ab8efdf9154a549167c58f2d42
Sebastian Soria <soriagorgoroso@gmail.com>
2022-05-26 17:17:20 -0300

    Event
    Primer modelo para mostrar un evento espesificio
    Se acepto que el creador del evento tenga que subir una foto para que se interprete que entrada corresponde a que sector.

---
+++ front/src/views/Events.jsx
@@ -0,0 +1,295 @@
+import React from "react";
+
+function Events() {
+  return (
+    <>
+      <div
+        className="breadcumb-area bg-img bg-overlay"
+        style={{ backgroundImage: "url(img/bg-img/breadcumb3.jpg)" }}
+      >
+        <div className="bradcumbContent">
+          <h2>Eventos</h2>
+        </div>
+      </div>
+      {/* <!-- ##### Breadcumb Area End ##### --> */}
+
+      {/* <!-- ##### Eventos Area Start ##### --> */}
+      <div className="Eventos-area section-padding-100">
+        <div className="container">
+          <div className="row">
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e1.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>Dj Night Party</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      VIP Sala
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e2.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>The Mission</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      Gold Arena
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e3.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>Planet ibiza</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      Space Ibiza
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e4.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>Dj Night Party</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      VIP Sala
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e5.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>The Mission</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      Gold Arena
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e6.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>Planet ibiza</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      Space Ibiza
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e7.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>Dj Night Party</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      VIP Sala
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e8.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>The Mission</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      Gold Arena
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Single Event Area --> */}
+            <div className="col-12 col-md-6 col-lg-4">
+              <div className="single-event-area mb-30">
+                <div className="event-thumbnail">
+                  <img src="img/bg-img/e9.jpg" alt=""/>
+                </div>
+                <div className="event-text">
+                  <h4>Planet ibiza</h4>
+                  <div className="event-meta-data">
+                    <a href="#" className="event-place">
+                      Space Ibiza
+                    </a>
+                    <a href="#" className="event-date">
+                      Mayo 15, 2022
+                    </a>
+                  </div>
+                  <a href="#" className="btn see-more-btn">
+                    Ver Evento
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12">
+              <div className="load-more-btn text-center mt-70">
+                <a href="#" className="btn oneMusic-btn">
+                  Más<i className="fa fa-angle-double-right"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- ##### Eventos Area End ##### --> */}
+
+      {/* <!-- ##### Nosotrosletter & Testimonials Area Start ##### --> */}
+      <div className="Nosotrosletter-testimonials-area">
+        <div className="container">
+          <div className="row">
+            {/* <!-- Nosotrosletter Area --> */}
+            <div className="col-12 col-lg-6">
+              <div className="Nosotrosletter-area mb-100">
+                <div className="section-heading text-left mb-50">
+                  <h2>Suscribirse</h2>
+                </div>
+                <div className="Nosotrosletter-form">
+                  <form action="#">
+                    <input type="search" name="search" id="NosotrosletterSearch" placeholder="E-mail"/>
+                    <button type="submit" className="btn oneMusic-btn">
+                      Subscribete <i className="fa fa-angle-double-right"></i>
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+            {/* <!-- Testimonials Area --> */}
+            <div className="col-12 col-lg-6">
+              <div
+                className="testimonials-area mb-100 bg-img bg-overlay"
+                style={{ backgroundImage: "url(img/bg-img/bg-3.jpg)" }}
+              >
+                <div className="section-heading white text-left mb-50">
+                  <h2>Testimoniales</h2>
+                </div>
+                {/* <!-- Testimonial Slide --> */}
+                <div className="testimonials-slide owl-carousel">
+                  {/* <!-- Single Slide --> */}
+                  <div className="single-slide">
+                    <p>Super recumendado </p>
+                    <div className="testimonial-info d-flex align-items-center">
+                      <div className="testimonial-thumb">
+                        <img src="img/bg-img/t1.jpg" alt=""/>
+                      </div>
+                      <p>William Smith, Cliente</p>
+                    </div>
+                  </div>
+                  {/* <!-- Single Slide --> */}
+                
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Events;
