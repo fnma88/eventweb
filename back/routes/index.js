3626a510e3f6abf8606d5305876318cd31683839
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-05-27 19:19:28 -0300

    routes

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,7 +1,14 @@
-const publicRoutes = require("./publicRoutes");
-const adminRoutes = require("./adminRoutes");
+const userRoutes = require("./userRoutes");
+const ticketRoutes = require("./ticketRoutes");
+const eventRoutes = require("./eventRoutes");
+const orderRoutes = require("./orderRoutes");
+const userController = require("../controllers/userController");
 
 module.exports = (app) => {
-  app.use(publicRoutes);
-  app.use("/admin", adminRoutes);
+  //Login
+  app.post("/tokens", userController.newToken);
+  app.use("/users", userRoutes);
+  app.use("/tickets", ticketRoutes);
+  app.use("/orders", orderRoutes);
+  app.use("/events", eventRoutes);
 };
