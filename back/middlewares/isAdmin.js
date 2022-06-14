8a9d8104a601c779ea6e1bae2f4d59be7d253a95
soriasebastian <soriagorgoroso@gmail.com>
2022-06-14 20:15:05 -0300

    Revert "try1"
    This reverts commit 9cb2c8bfae230d79b7961d47033a26b7841ed351.

--- back/middlewares/isAdmin.js
+++ back/middlewares/isAdmin.js
@@ -1,10 +1,10 @@
-// function isAdmin(req, res, next) {
-//   if (req.user.isAdmin) {
-//     console.log(JSON.stringify(req));
-//     return next();
-//   } else {
-//     return res.status(400).json({ message: "El usuario no es admin" });
-//   }
-// }
+function isAdmin(req, res, next) {
+  if (req.user.isAdmin) {
+    console.log(JSON.stringify(req));
+    return next();
+  } else {
+    return res.status(400).json({ message: "El usuario no es admin" });
+  }
+}
 
-// module.exports = isAdmin;
+module.exports = isAdmin;
